766ebe0f61ef65214a01b632201fe00108eba11a
kiwinam <mals930125@gmail.com>
2025-11-24 11:39:13 +0900

    fix: app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! Hi Everyone');
+  res.send('Hello, World! Hi Everyone  ==== KTB 3 기');
 });
 
 app.listen(port, () => {
